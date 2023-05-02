02019251aa014c7bcda7e5064118b33da6b8e8d4
samervalente <samervalente@gmail.com>
2023-05-02 11:31:26 -0300

    feat: create Header component

---
+++ components/Header.tsx
@@ -0,0 +1,33 @@
+import { House, Leaf } from "@phosphor-icons/react";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+
+export default function Header(){
+    const router = useRouter()
+    const [totalPoints, setTotalPoints] = useState()
+    
+    useEffect(() => {
+        const item: string | null = localStorage.getItem('totalPoints')
+        if(item){
+            setTotalPoints(JSON.parse(item))
+        }
+    },[])
+
+    return (
+        <div className="bg-green-500 flex flex-col  items-center justify-around p-3 text-white font-bold 
+        header-shadow">
+            <h1 className="flex items-center text-2xl my-4"><Leaf weight="fill" color="white" />BioQuizz</h1>
+            <div className="flex items-center justify-around w-full">
+                {router.asPath === '/quizz/anfibios' && <Link href={"/"} className="flex items-center gap-x-1">
+                    <House size={24} color="#fafafa" weight="fill" />
+                    Início
+                </Link>}
+            
+                {router.asPath === '/' && <div>
+                    Total de pontos: <span className="text-white-500 font-bold">{totalPoints}</span>
+                </div>}
+            </div>  
+        </div>
+    )
+}
