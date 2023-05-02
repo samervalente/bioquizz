8ca243775dd38069b4af527ff9053da5ec575f25
samervalente <samervalente@gmail.com>
2023-05-02 11:31:49 -0300

    chore: create question list constant

--- constants/questions.ts
+++ constants/questions.ts
@@ -2,49 +2,72 @@ import { Question } from "@/protocols/question";
 
 export const questions: Question[] = [
     {
+        id:1,
         title: 'Qual anfíbio não tem cauda na fase adulta?',
-        alternatives:[ "Sapo (X)", "Salamandra", "Cecília" ],
-        correctAlternative: 0
+        alternatives:[ "Sapo ", "Salamandra", "Cecília" ],
+        correctAlternative: 0,
+        
     },
     {
+        id:2,
         title: 'Quais são os animais que fazem parte do grupo dos anfíbios?',
-        alternatives:[ "Sapo, salamandra e cecília (X)", "Sapo, lagartixa e cecília", "Sapo, cobra e salamandra" ],
-        correctAlternative: 0
+        imageUrl: 'https://png.pngtree.com/element_our/20190604/ourlarge/pngtree-green-animal-frog-image_1473867.jpg',
+        alternatives:[ "Sapo, salamandra e cecília ", "Sapo, lagartixa e cecília", "Sapo, cobra e salamandra" ],
+        correctAlternative: 0,
+        
     },   {
+        id:3,
         title: 'Acerca da alimentação, a maiorida dos anfíbios são:',
-        alternatives:[ "Herbivoros", "Carnívoros (X)", "Onívoro", "Médias" ],
-        correctAlternative: 1
+        alternatives:[ "Herbivoros", "Carnívoros ", "Onívoro" ],
+        correctAlternative: 1,
+        
     },   {
+        id:4,
         title: 'As salamandras fazem parte de que grupo?',
-        alternatives:[ "Anura ou salientia", "Urodela ou caudata (X)", "Apoda ou gymnophiona" ],
-        correctAlternative: 1
+        alternatives:[ "Anura ou salientia", "Urodela ou caudata ", "Apoda ou gymnophiona" ],
+        correctAlternative: 1,
+        
     },   {
+        id:5,
         title: 'As cecílias fazem parte de que grupo?',
-        alternatives:[ "Apoda ou gymnophiona", "Urodela ou caudata", "Urodela ou caudata" ],
-        correctAlternative: 0
+        alternatives:[ "Apoda ou gymnophiona ", "Urodela ou caudata", "Anura ou salientia" ],
+        correctAlternative: 0,
+        
     },   {
+        id:6,
         title: 'Quais dos represententes do grupo anfíbia que possuem escamas internas?',
-        alternatives:[ "Sapos", "Pererecas", "Oxalote", "Salamandras", "Cecília (X)" ],
-        correctAlternative: 0
+        alternatives:[ "Sapos", "Pererecas", "Oxalote", "Salamandras", "Cecília" ],
+        correctAlternative: 4,
+        
     },   {
+        id:7,
         title: 'Quais algumas das sinapormorfias que juntam os grupos de anfíbios?',
-        alternatives:["Pele úmida, herbívoros, presença de pernas posteriores maiores" , "Pele úmida, respiração cutânea e carnívoros  (X)"],
-        correctAlternative: 1
+        alternatives:["Pele úmida, herbívoros, presença de pernas posteriores maiores" , "Pele úmida, respiração cutânea e carnívoros  "],
+        correctAlternative: 1,
+        
     },   {
+        id:8,
         title: 'Qual a estratégia na qual alguns anfíbios aproveitam a sua cor para se camuflar?',
-        alternatives:[ "Coloração cromada", "Coloração aposemática", "Coloração críptica (X)" ],
-        correctAlternative: 2
+        alternatives:[ "Coloração cromada", "Coloração aposemática", "Coloração críptica " ],
+        correctAlternative: 2,
+        
     },   {
+        id:9,
         title: 'Os anfíbios atuais fazem parte dos Lissamphybia?',
-        alternatives:[ "Falso", "Verdadeiro" ],
-        correctAlternative: 1
+        alternatives:[ "Falso", "Verdadeiro " ],
+        correctAlternative: 1,
+        
     },{
+        id:10,
         title: 'Qual é uma das caracteristicas que aproxima as cecílias dos outros grupos de anfíbios ',
-        alternatives:[ "A presença de patas", "Presença de dentes", "Tentáculos quimiossensoriais (X)" ],
-        correctAlternative: 2
+        alternatives:[ "A presença de patas", "Presença de dentes", "Tentáculos quimiossensoriais " ],
+        correctAlternative: 2,
+        
     },   {
+        id:11,
         title: 'Qual é o órgão de copula das cecílias?',
-        alternatives:[ "Clasper", "Protração", "Falodeu (X)" ],
-        correctAlternative: 2
+        alternatives:[ "Clasper", "Protração", "Falodeu " ],
+        correctAlternative: 2,
+        
     }
 ]
